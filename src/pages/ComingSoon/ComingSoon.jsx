4fab470348cdb0f3b0dc0b6e6a8e5ad734407ad9
unknown <industrialdesignoscar@gmail.com>
2023-03-19 18:32:48 -0500

    add: comingsoon section logo

--- src/pages/ComingSoon/ComingSoon.jsx
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './ComingSoon.scss';
+import creativityLogo from './../../assets/img/creativity.png';
 
 const ComingSoon = () => {
   return (
-    <div className='comingsoon'>ComingSoon...</div>
+    <div className='comingsoon'>
+      <img className='comingsoon__img' src={creativityLogo} alt="creative-logo" />
+      <p className='comingsoon__legend'>Estamos trabajando para ti.</p>
+      <p className='comingsoon__legend'>Próximamente ésta sección disponible...</p>
+      </div>
   )
 }
 
